3b517ced6c8a732bc73356e3721aafb07229cc86
Sahar <sal344@g.uky.edu>
2020-05-19 11:48:28 -0500

    Homework 3

---
+++ Corresponding to the Challenge PyPoll/main.py
@@ -0,0 +1,13 @@
+import csv
+
+# Path of the CSV file:
+input_path = "C:/Users/18594/Desktop/python-challenge/Corresponding to the Challenge PyPoll/election_data.csv"
+
+# Path of the output text file
+output_path = "C:/Users/18594/Desktop/python-challenge/Corresponding to the Challenge PyPoll/PyPoll_results.txt"
+
+
+
+print("Election Results")
+print("-------------------------")
+print(f"Total Votes: {}")
